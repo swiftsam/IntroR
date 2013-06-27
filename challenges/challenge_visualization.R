139e9b2c5a7424a53d66e25941ca4b58c600f481
Sam Swift <samswift@gmail.com>
2013-06-27 08:32:20 -0400

    adding visualization challenge

---
+++ challenges/challenge_visualization.R
@@ -0,0 +1,7 @@
+### CHALLENGE: visualization
+### Task:  Make a plot using a new dataset using at least 3 layers
+### Bonus: Use two geoms we haven't used yet
+### http://docs.ggplot2.org/current/
+
+data(mtcars)
+head(mtcars)
