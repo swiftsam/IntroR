3243be65e173e467b5e87457df8da07f5ed4446a
Sam Swift <samswift@gmail.com>
2013-06-27 08:27:06 -0400

    improving second ggplot examples and cleaning up end

--- modules/visualization.R
+++ modules/visualization.R
@@ -89,112 +89,35 @@ ggplot(movies, aes(x = year, y = budget)) +
        color = "Rating",
        linetype = "Short Film")
 
+### Some more ggplot examples
 
+head(iris) # Take a look at a new data set
 
-head(iris) # by default, head displays the first 6 rows
-head(iris, n = 10) # we can also explicitly set the number of rows to display
-
-#(The data frame actually contains three types of species: setosa, versicolor, and virginica.)
-
-#Let's plot `Sepal.Length` against `Petal.Length` using ggplot2's `qplot()` function.
-
+# Basic scatter plot
 ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
   geom_point()
-# Plot Sepal.Length vs. Petal.Length, using data from the `iris` data frame.
 
-#To see where each species is located in this graph, we can color each point by adding a `color = Species` argument.
-
+# Adding an aesthetic dimension
 ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
   geom_point(aes(color=Species)) # dude!
 
-#Similarly, we can let the size of each point denote sepal width, by adding a `size = Sepal.Width` argument.
-
+# And another
 ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
   geom_point(aes(color = Species, size = Petal.Width))
-# We see that Iris setosa flowers have the narrowest petals.
 
+# By setting the alpha of each point to 0.7, we reduce the effects of overplotting.
 ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
   geom_point(aes(color = Species, size = Petal.Width), alpha = 0.7)
-# By setting the alpha of each point to 0.7, we reduce the effects of overplotting.
 
+# Box plot to compare group means
+ggplot(iris, aes(x=Species, y=Petal.Length)) +
+  geom_boxplot()
 
-ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
-  geom_point(aes(color = Species, size = Petal.Width), alpha = 0.7) +
-  geom_smooth(method="lm", aes(color=Species))
-
-#Finally, let's fix the axis labels and add a title to the plot.
-ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) +
-  geom_point(aes(color = Species, size = Petal.Width), alpha = 0.7) +
+# Violin plot to represent the distributions more continously
+ggplot(iris, aes(x=Species, y=Petal.Length)) +
+  geom_violin() +
   labs(x = "Sepal Length", 
        y = "Petal Length", 
        title = "Sepal vs. Petal Length in Fisher's Iris data")
-
-
-## Other common geoms
-
-#In the scatterplot examples above, we implicitly used a *point* **geom**, the default when you supply two arguments to `qplot()`.
-
-# These two invocations are equivalent.
-qplot(Sepal.Length, Petal.Length, data = iris, geom = "point")
-qplot(Sepal.Length, Petal.Length, data = iris)
-
-
-### Line charts: geom = "line"
-
-qplot(Sepal.Length, Petal.Length, data = iris, geom = "line", color = Species) 
-# Using a line geom doesn't really make sense here, but hey.
-
-# `Orange` is another built-in data frame that describes the growth of orange trees.
-qplot(age, circumference, data = Orange, geom = "line",
-      colour = Tree,
-      main = "How does orange tree circumference vary with age?")
-
-
-data(mtcars)
-head(mtcars)
-mtcars$names <- rownames(mtcars)
-
-# Plots of Frequency x category (Bar Chart)
-qplot(factor(cyl), data=mtcars, geom="bar") # with qplot
-
-ggplot(mtcars, aes(factor(cyl))) +          # with ggplot
-  geom_bar() 
-
-ggplot(mtcars, aes(factor(cyl))) +          # with ggplot and extra tweaks
-  geom_bar(fill="white", color="darkgreen") +
-  xlab("# of Cylinders") +
-  opts(title="Count of Models by # of Cylinders")
-
-# Plots of distribution x category (Box Plot, Violin Plot)
-qplot(factor(cyl), mpg, data = mtcars, geom = "boxplot")   #with qplot
-
-ggplot(mtcars, aes(factor(cyl),mpg)) +          # with ggplot
-  geom_boxplot()
-
-ggplot(mtcars, aes(factor(cyl),mpg)) +          # with ggplot and extras
-  geom_violin() +
-  geom_jitter(color="red") 
-
-ggplot(mtcars, aes(factor(cyl),mpg)) +          # with ggplot and extras
-  geom_violin() +
-  geom_jitter(aes(color=hp)) +
-  geom_text(aes(label=names), size=2)
-
-# Plots of relationship 
-qplot(mpg, hp, data=mtcars, geom="point")      #with qplot
-
-ggplot(mtcars, aes(mpg,hp)) +                 #with ggplot
-  geom_point()
-
-ggplot(mtcars, aes(mpg,hp)) +                 # with ggplot and extras
-  geom_point(aes(color=disp)) +
-  geom_smooth(method="lm")
-
-ggplot(mtcars, aes(mpg,hp)) +                 # with ggplot and extras
-  geom_point(aes(color=disp)) +
-  geom_smooth(method="loess")
-
-
-
 
 
